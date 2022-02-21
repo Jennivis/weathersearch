e952f4bc728034f8c530f6c127c2c044a706e293
Jennivis <n1vije00@students.oamk.fi>
2022-02-21 21:29:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, {  useState } from 'react';
 import './App.css';
 
+const API_KEY = ""
+const ICON_URL = "https://openweathermap.org/img/wn/";
+
 function App() {
+
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("0");
+  const [feelsLike, setFeelsLike] = useState ("0");
+  const [windSpeed, setWindSpeed] = useState("0");
+  const [description, setDescription] = useState("");
+  const [icon, setIcon] = useState("");
+
+  const getWeatherData = (city) => {
+    axios({
+      method: "GET",
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}&lang=fi`
+    })
+      .then((response) => {
+      console.log(response.data.main.temp);
+      setTemp(response.data.main.temp);
+      setWindSpeed(response.data.wind.speed);
+      setFeelsLike(response.data.main.feels_like);
+      setDescription(response.data.weather[0].description);
+      setIcon(ICON_URL + response.data.weather[0].icon + "@2x.png");
+    }).catch (error => {
+      alert(error + ". Jokin meni pieleen... Paikkakunta puuttuu tai on virheellinen.");
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <h2>Hae päivän säätiedot</h2>
+    <div className="container">
+      <div className="card">
+        <input type="text" className="search-city" placeholder="Syötä paikkakunta" value={city} onChange={(e) => setCity(e.target.value)}/>
+        <button className="btn" onClick={() => {
+          getWeatherData(city);
+        }}>Hae</button>
+        <div className="results">
+          <p className="city">{city}</p>
+          <p className="temp">Lämpötila: {Math.round(temp)} &#176;C</p>
+          <p>Tuulen voimakkuus {Math.round(windSpeed)} m/s</p>
+          <p>Tuntuu kuin {Math.round(feelsLike)} &#176;C</p>
+          <p className="desc">{description}</p>
+          <img src={icon} alt=""/>
+        </div>
+      </div>
     </div>
+  </>   
   );
 }
 
